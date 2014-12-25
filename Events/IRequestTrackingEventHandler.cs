7a056b769077032d7a6e3c033ec52b8d90b072cf
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-12-25 23:09:12 +0100

    Implementing request tracking

---
+++ Events/IRequestTrackingEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.DataContracts;
+using Orchard.Events;
+
+namespace Lombiq.Hosting.Azure.ApplicationInsights.Events
+{
+    /// <summary>
+    /// Exposes events for HTTP request tracking.
+    /// </summary>
+    public interface IRequestTrackingEventHandler : IEventHandler
+    {
+        /// <summary>
+        /// Fires when the HTTP request begins.
+        /// </summary>
+        /// <param name="requestTelemetry">The telemetry object for the request, containing pre-filled data.</param>
+        void OnBeginRequest(RequestTelemetry requestTelemetry);
+
+        /// <summary>
+        /// Fires when the HTTP request ends.
+        /// </summary>
+        /// <param name="requestTelemetry">The telemetry object for the request, containing pre-filled data.</param>
+        void OnEndRequest(RequestTelemetry requestTelemetry);
+    }
+}
---
+++ Services/RequestTrackingMiddleware.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Logging;
+using Orchard.Owin;
+using Owin;
+using Microsoft.Owin.Extensions;
+using Lombiq.Hosting.Azure.ApplicationInsights.Models;
+using Orchard;
+using Orchard.Mvc;
+using Orchard.Services;
+using Microsoft.ApplicationInsights.DataContracts;
+using Lombiq.Hosting.Azure.ApplicationInsights.Events;
+
+namespace Lombiq.Hosting.Azure.ApplicationInsights.Services
+{
+    /// <summary>
+    /// Owin middleware for tracking request metrics through Application Insights.
+    /// </summary>
+    public class RequestTrackingMiddleware : IOwinMiddlewareProvider
+    {
+        private readonly IWorkContextAccessor _wca;
+
+
+        public RequestTrackingMiddleware(IWorkContextAccessor wca)
+        {
+            _wca = wca;
+        }
+
+
+        public IEnumerable<OwinMiddleware> GetOwinMiddlewares()
+        {
+            return new[]
+            {
+                new OwinMiddleware
+                {
+                    Configure = app =>
+                        {
+                            app.Use(async (context, next) =>
+                            {
+                                var workContext = _wca.GetContext();
+                                var clock = workContext.Resolve<IClock>();
+
+                                var requestStart = clock.UtcNow;
+
+                                var httpContext = workContext.Resolve<IHttpContextAccessor>().Current();
+                                var httpRequest = httpContext.Request;
+                                var httpResponse = httpContext.Response;
+                                var requestTrackingEvents = workContext.Resolve<IRequestTrackingEventHandler>();
+
+
+                                var requestTelemetry = new RequestTelemetry
+                                {
+                                    Timestamp = requestStart,
+                                    Url = httpRequest.Url,
+                                    HttpMethod = httpRequest.HttpMethod
+                                };
+                                requestTelemetry.Context.Location.Ip = httpRequest.UserHostAddress;
+
+                                requestTrackingEvents.OnBeginRequest(requestTelemetry);
+
+                                await next.Invoke();
+
+                                requestTelemetry.Duration = clock.UtcNow - requestStart;
+                                requestTelemetry.ResponseCode = httpResponse.StatusCode.ToString();
+                                requestTelemetry.Success = httpResponse.StatusCode < 400;
+                                requestTelemetry.Name = workContext.Layout.Title; 
+
+                                requestTrackingEvents.OnEndRequest(requestTelemetry);
+
+                                workContext.Resolve<ITelemetryClientFactory>().CreateTelemetryClientFromDefaultConfiguration().TrackRequest(requestTelemetry);
+                            });
+                        }
+                }
+            };
+        }
+    }
+}
