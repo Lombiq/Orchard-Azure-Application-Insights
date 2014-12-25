70c008c9ec24bdeb93130cf9f860e2ddb59aefc6
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-12-25 20:02:55 +0100

    Adding on the fly assembly redirect for redirects required by AI libraries

--- Readme.md
+++ Readme.md
@@ -4,4 +4,9 @@
 
 This Orchard module enables easy integration of [Azure Application Insights](http://azure.microsoft.com/en-us/documentation/articles/app-insights-start-monitoring-app-health-usage/) telemetry.
 
+
+## Updating AI libraries
+
 Application Insights NuGet packages are included in the Lib folder. If you want to update them just open the [Hosting - Azure Application Insights Packages](https://bitbucket.org/Lombiq/hosting-azure-application-insights-packages) project, update them there through the NuGet UI and copy over the updated libraries. Note that libraries, unlike how they are created by NuGet, should go into a subfolder whose name doesn't contain their version, to simplify updates. Make sure to apply any changes that are necessary to be made on the client code (like after API changes).
+
+When assembly binding redirects are changed make sure to also edit `AssemblyRedirectSetupShellEventHandler` that mimicks such redirects instead of relying on the Web.config.
---
+++ Services/AssemblyRedirectSetupShellEventHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Orchard.Environment;
+using Orchard.Environment.Configuration;
+
+namespace Lombiq.Hosting.Azure.ApplicationInsights.Services
+{
+    public class AssemblyRedirectSetupShellEventHandler : IOrchardShellEvents
+    {
+        public void Activated()
+        {
+            // Trying to remove first so no duplicate event registration can occur.
+            AppDomain.CurrentDomain.AssemblyResolve -= ResolveApplicationInsightsAssemblies;
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveApplicationInsightsAssemblies;
+        }
+
+        public void Terminating()
+        {
+        }
+
+
+        /// <summary>
+        /// Redirecting assembly resolutions for AI-related assemblies. This is instead of putting assembly redirects into the Web.config.
+        /// </summary>
+        private Assembly ResolveApplicationInsightsAssemblies(object sender, ResolveEventArgs args)
+        {
+            /*
+               Curently the following redirects are mimicked here (edit these when necessary after updating AI libraries):
+                <dependentAssembly>
+                  <assemblyIdentity name="Microsoft.Diagnostics.Tracing.EventSource" publicKeyToken="b03f5f7f11d50a3a" culture="neutral" />
+                  <bindingRedirect oldVersion="0.0.0.0-1.1.14.0" newVersion="1.1.14.0" />
+                </dependentAssembly>
+                <dependentAssembly>
+                  <assemblyIdentity name="Microsoft.ApplicationInsights" publicKeyToken="31bf3856ad364e35" culture="neutral" />
+                  <bindingRedirect oldVersion="0.0.0.0-0.12.0.17386" newVersion="0.12.0.17386" />
+                </dependentAssembly>
+             */
+
+            var assemblyShortName = args.Name.Split(',')[0];
+
+            switch (assemblyShortName)
+            {
+                case "Microsoft.Diagnostics.Tracing.EventSource":
+                    return typeof(Microsoft.Diagnostics.Tracing.EventAttribute).Assembly;
+                case "Microsoft.ApplicationInsights":
+                    return typeof(Microsoft.ApplicationInsights.TelemetryClient).Assembly;
+                default:
+                    return null;
+            }
+        }
+    }
+}
