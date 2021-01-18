6af917a9ee81f96bd8aca2241d6cfabefbdb0f52
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-01-19 00:52:08 +0100

    Working background task telemetry collection

--- Services/BackgroundTaskTelemetryDecorator.cs
+++ Services/BackgroundTaskTelemetryDecorator.cs
@@ -1,13 +1,9 @@
 using Microsoft.ApplicationInsights;
-using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
-using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.BackgroundTasks;
 using OrchardCore.Modules;
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,8 +19,14 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Services
         {
             var telemetryClient = serviceProvider.GetRequiredService<TelemetryClient>();
 
-            using var operation = telemetryClient.StartOperation<BackgroundOperationTelemetry>(_decorated.GetType().FullName);
-            //operation.Telemetry.Type = "Background";
+            // Tried with a custom operation too, see c13dfdc47543d8635572be1f2ce1191dba06469b, but it remained
+            // "unconfigured", i.e. lacking the instrumentation key in the Context, and never getting sent to Azure.
+            // Note that while the operation is DependencyTelemetry it'll be collected even if
+            // EnableDependencyTrackingTelemetryModule is false. Also, this is the recommended way of tracking
+            // background tasks, see:
+            // https://docs.microsoft.com/en-us/azure/azure-monitor/app/custom-operations-tracking#long-running-background-tasks.
+            using var operation = telemetryClient.StartOperation<DependencyTelemetry>(_decorated.GetType().FullName);
+            operation.Telemetry.Type = "Background";
 
             try
             {
@@ -34,64 +36,6 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Services
             {
                 // Need to catch the exception to correlate it with the background operation.
                 telemetryClient.TrackException(ex);
-            }
-        }
-
-        private class BackgroundOperationTelemetry : OperationTelemetry
-        {
-            private BackgroundTaskData _data;
-
-            public override string Id { get; set; }
-            public override string Name { get; set; }
-            public override bool? Success { get; set; }
-            public override TimeSpan Duration { get; set; }
-            public override IDictionary<string, double> Metrics => new Dictionary<string, double>();
-            public override IDictionary<string, string> Properties => new Dictionary<string, string>();
-            public override DateTimeOffset Timestamp { get; set; }
-            public override TelemetryContext Context => new();
-            public override string Sequence { get; set; }
-            public override IExtension Extension { get; set; }
-
-            private BackgroundTaskData Data =>
-                LazyInitializer.EnsureInitialized(
-                    ref _data,
-                    () => new BackgroundTaskData
-                    {
-                        Duration = Duration,
-                        Id = Id,
-                        Metrics = Metrics,
-                        Name = Name,
-                        Properties = Properties,
-                        Success = Success,
-                    });
-
-            public override ITelemetry DeepClone() => throw new NotSupportedException();
-
-            public override void SerializeData(ISerializationWriter serializationWriter)
-            {
-                // Following the pattern of what's in e.g. DependencyTelemetry.
-                _data = null;
-                serializationWriter.WriteProperty(Data);
-            }
-
-            private class BackgroundTaskData : ISerializableWithWriter
-            {
-                public string Id { get; set; }
-                public string Name { get; set; }
-                public bool? Success { get; set; }
-                public TimeSpan Duration { get; set; }
-                public IDictionary<string, double> Metrics { get; set; } = new Dictionary<string, double>();
-                public IDictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
-
-                public void Serialize(ISerializationWriter serializationWriter)
-                {
-                    serializationWriter.WriteProperty("Id", Id);
-                    serializationWriter.WriteProperty("Name", Name);
-                    serializationWriter.WriteProperty("Duration", Duration);
-                    serializationWriter.WriteProperty("Success", Success);
-                    serializationWriter.WriteProperty("Properties", Properties);
-                    serializationWriter.WriteProperty("Metrics", Metrics);
-                }
             }
         }
     }
