26759cc65bec48d51e6e088284eb6a1eba818f31
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-01-28 14:30:16 +0100

    Merging ifs

--- Services/TrackingScriptInjectingFilter.cs
+++ Services/TrackingScriptInjectingFilter.cs
@@ -21,39 +21,36 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Services
 
         public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
         {
-            if (context.IsNotFullViewRendering())
+            if (context.IsNotFullViewRendering() || !_applicationInsightsOptions.Value.EnableClientSideTracking)
             {
                 await next();
                 return;
             }
 
-            if (_applicationInsightsOptions.Value.EnableClientSideTracking)
+            if (_applicationInsightsOptions.Value.EnableOfflineOperation)
             {
-                if (_applicationInsightsOptions.Value.EnableOfflineOperation)
-                {
-                    var offlineScript = new HtmlString(
-                        $@"<script>
-                            appInsights = 'enabled';
-                        </script>");
-                    _resourceManager.RegisterHeadScript(offlineScript);
-                }
-                else
-                {
-                    _resourceManager.RegisterResource("script", ResourceNames.TrackingScript).AtHead();
+                var offlineScript = new HtmlString(
+                    $@"<script>
+                        appInsights = 'enabled';
+                    </script>");
+                _resourceManager.RegisterHeadScript(offlineScript);
+            }
+            else
+            {
+                _resourceManager.RegisterResource("script", ResourceNames.TrackingScript).AtHead();
 
-                    // The operation ID is NOT available in the injectable TelemetryClient, which will be basically
-                    // empty. This is somehow by design. See e.g.:
-                    // https://stackoverflow.com/questions/39149815/when-can-i-get-an-application-insights-operation-id
-                    var operationId = System.Diagnostics.Activity.Current.RootId;
+                // The operation ID is NOT available in the injectable TelemetryClient, which will be basically
+                // empty. This is somehow by design. See e.g.:
+                // https://stackoverflow.com/questions/39149815/when-can-i-get-an-application-insights-operation-id
+                var operationId = System.Diagnostics.Activity.Current.RootId;
 
-                    var correlationScript = new HtmlString(
-                        $@"<script>
-                            appInsights.queue.push(function () {{
-                                appInsights.properties.context.telemetryTrace.traceID = '{operationId}';
-                            }});
-                        </script>");
-                    _resourceManager.RegisterHeadScript(correlationScript);
-                }
+                var correlationScript = new HtmlString(
+                    $@"<script>
+                        appInsights.queue.push(function () {{
+                            appInsights.properties.context.telemetryTrace.traceID = '{operationId}';
+                        }});
+                    </script>");
+                _resourceManager.RegisterHeadScript(correlationScript);
             }
 
             await next();
