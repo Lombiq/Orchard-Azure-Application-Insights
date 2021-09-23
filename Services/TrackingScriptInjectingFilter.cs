174dc5eb7c9c2ed85c07e4dab5475948245010e8
Ferenc Győri <ferenc.gyori@lombiq.com>
2021-09-23 16:59:41 +0200

    Checking ITrackingConsentFeature in TrackingScriptInjectingFilter

--- Services/TrackingScriptInjectingFilter.cs
+++ Services/TrackingScriptInjectingFilter.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Options;
 using OrchardCore.ResourceManagement;
@@ -11,15 +13,18 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Services
         private readonly IResourceManager _resourceManager;
         private readonly IOptions<ApplicationInsightsOptions> _applicationInsightsOptions;
         private readonly ITrackingScriptFactory _trackingScriptFactory;
+        private readonly IHttpContextAccessor _hca;
 
         public TrackingScriptInjectingFilter(
             IResourceManager resourceManager,
             IOptions<ApplicationInsightsOptions> applicationInsightsOptions,
-            ITrackingScriptFactory trackingScriptFactory)
+            ITrackingScriptFactory trackingScriptFactory,
+            IHttpContextAccessor hca)
         {
             _resourceManager = resourceManager;
             _applicationInsightsOptions = applicationInsightsOptions;
             _trackingScriptFactory = trackingScriptFactory;
+            _hca = hca;
         }
 
         public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
@@ -30,18 +35,28 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Services
                 return;
             }
 
-            if (_applicationInsightsOptions.Value.EnableOfflineOperation)
+            var trackingConsentFeature = _hca.HttpContext.Features.Get<ITrackingConsentFeature>();
+
+            if (trackingConsentFeature is null || trackingConsentFeature.CanTrack)
             {
-                var offlineScript = new HtmlString(
-                    $@"<script>
+                if (_applicationInsightsOptions.Value.EnableOfflineOperation)
+                {
+                    var offlineScript = new HtmlString(
+                        $@"<script>
                         appInsights = 'enabled';
                     </script>");
-                _resourceManager.RegisterHeadScript(offlineScript);
+                    _resourceManager.RegisterHeadScript(offlineScript);
+                }
+                else
+                {
+                    _resourceManager.RegisterHeadScript(_trackingScriptFactory.CreateJavaScriptTrackingScript());
+                }
             }
-            else
-            {
-                _resourceManager.RegisterHeadScript(_trackingScriptFactory.CreateJavaScriptTrackingScript());
-            }
+
+            // In the else branch we could delete the cookie that allows client side tracking.
+            // These provide a solution to handle the following cases:
+            // -The configuration of ITrackingConsentFeature changes after this module is enabled.(e.g. Lombiq.Privacy)
+            // -A browser is used by multiple users and not all of them have accepted the privacy policy.
 
             await next();
         }
