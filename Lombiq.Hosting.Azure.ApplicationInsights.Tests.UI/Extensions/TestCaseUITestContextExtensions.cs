a63ac55481231068e7e703259b9d53f665c40bef
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-08-23 22:22:01 +0200

    Adding UI test project

---
+++ Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -0,0 +1,23 @@
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using Shouldly;
+
+namespace Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI.Extensions;
+
+public static class TestCaseUITestContextExtensions
+{
+    /// <summary>
+    /// Tests Application Insights tracking during offline operation. Use <see
+    /// cref="OrchardCoreUITestExecutorConfigurationExtensions.EnableApplicationInsightsOfflineOperation"/> to switch
+    /// the module into offline mode.
+    /// </summary>
+    public static void TestApplicationInsightsTrackingInOfflineOperation(this UITestContext context)
+    {
+        var appInsightsExist = context.ExecuteScript("return window.appInsights === 'enabled'") as bool?;
+
+        // Our custom message helps debugging, otherwise from the test output you could only tell that a value should be
+        // true but is false which is less than helpful.
+        appInsightsExist.ShouldBe(expected: true, "The Application Insights module is not working or is not in offline mode.");
+    }
+}
---
+++ Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI/Readme.md
@@ -0,0 +1,7 @@
+# Lombiq Hosting - Azure Application Insights for Orchard Core - UI Test Extensions
+
+## About
+
+Extension methods that test various features in Lombiq Hosting - Azure Application Insights for Orchard Core, with the help of [Lombiq UI Testing Toolbox for Orchard Core](https://github.com/Lombiq/UI-Testing-Toolbox).
+
+Call these from a UI test project to verify the module's basic features; as seen in [Open-Source Orchard Core Extensions](https://github.com/Lombiq/Open-Source-Orchard-Core-Extensions).
