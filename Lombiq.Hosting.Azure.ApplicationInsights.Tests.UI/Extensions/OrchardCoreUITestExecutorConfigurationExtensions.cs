ed9f2cf7647b966f4dac6c6edb11ed6e016155fc
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-08-25 22:30:40 +0200

    Throwing exception if the AI offline mode wasn't enabled

--- Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI/Extensions/OrchardCoreUITestExecutorConfigurationExtensions.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI/Extensions/OrchardCoreUITestExecutorConfigurationExtensions.cs
@@ -5,12 +5,17 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI.Extensions;
 
 public static class OrchardCoreUITestExecutorConfigurationExtensions
 {
+    internal static bool IsOfflineOperationEnabled { get; private set; }
+
     /// <summary>
     /// Adds a command line argument to the app during <see cref="OrchardCoreConfiguration.BeforeAppStart"/> that
     /// switches the Application Insights module into offline mode. This way it won't try to reach out to a remote
     /// server with telemetry and the test remains self-contained.
     /// </summary>
-    public static void EnableApplicationInsightsOfflineOperation(this OrchardCoreUITestExecutorConfiguration configuration) =>
+    public static void EnableApplicationInsightsOfflineOperation(this OrchardCoreUITestExecutorConfiguration configuration)
+    {
+        IsOfflineOperationEnabled = true;
+
         configuration.OrchardCoreConfiguration.BeforeAppStart +=
             (_, argumentsBuilder) =>
             {
@@ -20,4 +25,5 @@ public static class OrchardCoreUITestExecutorConfigurationExtensions
 
                 return Task.CompletedTask;
             };
+    }
 }
--- Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -2,6 +2,7 @@ using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
 using Shouldly;
+using System;
 
 namespace Lombiq.Hosting.Azure.ApplicationInsights.Tests.UI.Extensions;
 
@@ -14,6 +15,15 @@ public static class TestCaseUITestContextExtensions
     /// </summary>
     public static void TestApplicationInsightsTrackingInOfflineOperation(this UITestContext context)
     {
+        if (!OrchardCoreUITestExecutorConfigurationExtensions.IsOfflineOperationEnabled)
+        {
+            throw new InvalidOperationException(
+                "Testing Application Insights tracking during offline operation only works if you switch the module " +
+                "into offline mode by calling " +
+                "OrchardCoreUITestExecutorConfigurationExtensions.EnableApplicationInsightsOfflineOperation before " +
+                "the app starts.");
+        }
+
         var appInsightsExist = context.ExecuteScript("return window.appInsights === 'enabled'") as bool?;
 
         // Our custom message helps debugging, otherwise from the test output you could only tell that a value should be
