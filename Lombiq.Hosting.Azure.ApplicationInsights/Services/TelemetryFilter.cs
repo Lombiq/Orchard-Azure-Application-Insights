d3216314d2e761db95222f9c18d39c1fd85f8aec
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-04 17:52:03 +0200

    Filter expected dependency errors

--- Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
@@ -1,36 +1,35 @@
 ﻿using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
+using System.Collections.Generic;
 
 namespace Lombiq.Hosting.Azure.ApplicationInsights.Services;
 
 public class TelemetryFilter : ITelemetryProcessor
 {
-    private ITelemetryProcessor _next;
-    public TelemetryFilter(ITelemetryProcessor next)
+    private readonly List<string> _errors = new()
     {
+        "Azure.RequestFailedException: The specified container already exists.",
+        "The specified blob does not exist.",
+        "Microsoft.Data.SqlClient.SqlException (0x80131904): There is already an object named",
+    };
+    private readonly ITelemetryProcessor _next;
+
+    public TelemetryFilter(ITelemetryProcessor next) =>
         _next = next;
-    }
 
     public void Process(ITelemetry item)
     {
-        // To filter out an item, return without calling the next processor.
         if (!OKtoSend(item)) { return; }
 
         _next.Process(item);
     }
 
-    private bool OKtoSend (ITelemetry item)
+    private bool OKtoSend(ITelemetry item)
     {
         var dependency = item as DependencyTelemetry;
         if (dependency is not { Success: false }) return true;
 
-        if (dependency.Name == "BlobBaseClient.Exists" ||
-            dependency.Name == "Blob.GetProperties" ||
-            (dependency.Type == "SQL" && dependency.ResultCode == "2714"))
-        {
-            var asd = "asd";
-        }
-        return true;
+        return !_errors.Contains(dependency.Properties["Error"]) && !_errors.Contains(dependency.Properties["Exception"]);
     }
 }
