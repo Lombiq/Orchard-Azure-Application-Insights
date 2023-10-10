84892010bca628a12c12f39381751a7715c0c536
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-10 12:50:34 +0200

    Improving filter

--- Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
@@ -2,21 +2,22 @@
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 using System.Collections.Generic;
+using YesSql.Services;
 
 namespace Lombiq.Hosting.Azure.ApplicationInsights.Services;
 
 public class TelemetryFilter : ITelemetryProcessor
 {
-    private readonly List<string> _errors = new()
+    private static readonly List<string> Errors = new()
     {
         "Azure.RequestFailedException: The specified container already exists.",
         "The specified blob does not exist.",
-        "Microsoft.Data.SqlClient.SqlException (0x80131904): There is already an object named",
+        "Microsoft.Data.SqlClient.SqlException (0x80131904): There is already an object named 'Shells_Identifiers' in the database.",
     };
+
     private readonly ITelemetryProcessor _next;
 
-    public TelemetryFilter(ITelemetryProcessor next) =>
-        _next = next;
+    public TelemetryFilter(ITelemetryProcessor next) => _next = next;
 
     public void Process(ITelemetry item)
     {
@@ -25,13 +26,21 @@ public class TelemetryFilter : ITelemetryProcessor
         _next.Process(item);
     }
 
-    private bool ShouldSend(ITelemetry item)
+    private static bool ShouldSend(ITelemetry item)
     {
         var dependency = item as DependencyTelemetry;
         if (dependency is not { Success: false }) return true;
 
         dependency.Properties.TryGetValue("Error", out var error);
         dependency.Properties.TryGetValue("Exception", out var exception);
-        return !_errors.Contains(error) && !_errors.Contains(exception);
+        dependency.Properties.TryGetValue("OrchardCore.ShellName", out var shellName);
+        var shouldSend = !(error.IsIn(Errors) ||
+            exception.IsIn(Errors) ||
+            exception?.Contains(GetTenantSqlErrorMessage(shellName)) == true);
+
+        return shouldSend;
     }
+
+    private static string GetTenantSqlErrorMessage(string tenantName) =>
+        $"Microsoft.Data.SqlClient.SqlException (0x80131904): There is already an object named '{tenantName}_Identifiers' in the database.";
 }
