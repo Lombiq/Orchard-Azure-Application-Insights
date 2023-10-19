e092ba4ee06e6bc0714a80c9a0b6f80b97867bdb
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-19 16:40:17 +0200

    Cleaning the logic

--- Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
@@ -8,13 +8,13 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Services;
 
 public class TelemetryFilter : ITelemetryProcessor
 {
-    private static readonly List<string> _expectedErrors = new()
+    private static readonly List<string> _expectedErrorsAsRegexPattern = new()
     {
         // Using this generic error message to filter because it is not possible to filter only those cases where it is
         // meant to happen to throw this error. Also setting CreateContainer to false won't solve this issue, because
         // Blob Media container is always checked on each tenant activation.
-        "Azure.RequestFailedException: The specified container already exists.",
-        "Microsoft.Data.SqlClient.SqlException (0x80131904): There is already an object named 'Shells_Identifiers' in the database.",
+        @"Azure\.RequestFailedException: The specified container already exists\.",
+        @"Microsoft\.Data\.SqlClient\.SqlException \(0x80131904\): There is already an object named '.*_Identifiers' in the database\.",
     };
 
     private readonly ITelemetryProcessor _next;
@@ -23,37 +23,31 @@ public class TelemetryFilter : ITelemetryProcessor
 
     public void Process(ITelemetry item)
     {
-        if (!ShouldSend(item)) { return; }
+        if (ShouldNotSend(item)) { return; }
 
         _next.Process(item);
     }
 
-    private static bool ShouldSend(ITelemetry item)
+    private static bool ShouldNotSend(ITelemetry item)
     {
         var dependency = item as DependencyTelemetry;
-        if (dependency is not { Success: false }) return true;
+        if (dependency is not { Success: false }) return false;
 
         dependency.Properties.TryGetValue("Error", out var error);
         dependency.Properties.TryGetValue("Exception", out var exception);
-        dependency.Properties.TryGetValue("OrchardCore.ShellName", out var shellName);
-        var hasError = _expectedErrors.Exists(expectedError => error?.StartsWithOrdinalIgnoreCase(expectedError) == true) ||
-            _expectedErrors.Exists(expectedError => exception?.StartsWithOrdinalIgnoreCase(expectedError) == true) ||
-            exception?.Contains(GetTenantSqlErrorMessage(shellName)) == true;
-        var shouldSend = !hasError;
 
-        if (!shouldSend)
+        var hasError = _expectedErrorsAsRegexPattern.Exists(expectedError => error?.RegexIsMatch(expectedError) == true) ||
+            _expectedErrorsAsRegexPattern.Exists(expectedError => exception?.RegexIsMatch(expectedError) == true);
+
+        if (hasError)
         {
-            return false;
+            return true;
         }
 
         dependency.Properties.TryGetValue("OrchardCore.DataProtectionContainerName", out var dataProtectionContainerName);
         dependency.Properties.TryGetValue("OrchardCore.MediaBlobStorageContainerName", out var mediaBlobStorageContainerName);
-        shouldSend = !(dependency.Name == "PUT " + dataProtectionContainerName ||
-            dependency.Name == "PUT " + mediaBlobStorageContainerName);
 
-        return shouldSend;
+        return dependency.Name == "PUT " + dataProtectionContainerName ||
+            dependency.Name == "PUT " + mediaBlobStorageContainerName;
     }
-
-    private static string GetTenantSqlErrorMessage(string tenantName) =>
-        $"Microsoft.Data.SqlClient.SqlException (0x80131904): There is already an object named '{tenantName}_Identifiers' in the database.";
 }
