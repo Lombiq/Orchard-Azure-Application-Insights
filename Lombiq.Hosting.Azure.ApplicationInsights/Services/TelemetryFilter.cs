ddb444dfddc7c102b35e74d51ef2b8f9d6427988
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-19 16:44:26 +0200

    Extending docs

--- Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
@@ -12,7 +12,8 @@ public class TelemetryFilter : ITelemetryProcessor
     {
         // Using this generic error message to filter because it is not possible to filter only those cases where it is
         // meant to happen to throw this error. Also setting CreateContainer to false won't solve this issue, because
-        // Blob Media container is always checked on each tenant activation.
+        // Blob Media container is always checked on each tenant activation in OrchardCore
+        // MediaBlobContainerTenantEvents class.
         @"Azure\.RequestFailedException: The specified container already exists\.",
         @"Microsoft\.Data\.SqlClient\.SqlException \(0x80131904\): There is already an object named '.*_Identifiers' in the database\.",
     };
