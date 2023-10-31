aaebf499ec6938837a3fc39f426abd8f65d9a1c0
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-31 14:39:24 +0100

    Moving logic to extension

---
+++ Lombiq.Hosting.Azure.ApplicationInsights/Extensions/ApplicationInsightsInitializerExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Lombiq.Hosting.Azure.ApplicationInsights;
+using Lombiq.Hosting.Azure.ApplicationInsights.Services;
+using Lombiq.Hosting.Azure.ApplicationInsights.TelemetryInitializers;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ApplicationInsightsInitializerExtensions
+{
+    /// <summary>
+    /// Use this extension method to initialize Application Insights. Should be used in the application Program.cs file.
+    /// </summary>
+    public static IServiceCollection InitializeApplicationInsightsTelemetry(
+        this IServiceCollection services,
+        ConfigurationManager configurationManager)
+    {
+        services.AddApplicationInsightsTelemetry(configurationManager);
+        services.AddApplicationInsightsTelemetryProcessor<TelemetryFilter>();
+
+        // Since the below AI configuration needs to happen during app startup in ConfigureServices() we can't use an
+        // injected IOptions<T> here but need to directly bind to ApplicationInsightsOptions.
+        var options = new ApplicationInsightsOptions();
+        var configSection = configurationManager.GetSection("Lombiq_Hosting_Azure_ApplicationInsights");
+        configSection.Bind(options);
+        services.Configure<ApplicationInsightsOptions>(configSection);
+
+        services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>(
+            (module, _) => module.EnableSqlCommandTextInstrumentation = options.EnableSqlCommandTextInstrumentation);
+
+        services.ConfigureTelemetryModule<QuickPulseTelemetryModule>(
+            (module, _) => module.AuthenticationApiKey = options.QuickPulseTelemetryModuleAuthenticationApiKey);
+
+        services.AddSingleton<ITelemetryInitializer, UserContextPopulatingTelemetryInitializer>();
+        services.AddSingleton<ITelemetryInitializer, ShellNamePopulatingTelemetryInitializer>();
+        services.Configure<MvcOptions>((options) => options.Filters.Add(typeof(TrackingScriptInjectingFilter)));
+        services.AddScoped<ITrackingScriptFactory, TrackingScriptFactory>();
+
+        if (options.EnableOfflineOperation)
+        {
+            foreach (var descriptor in services.Where(descriptor => descriptor.ServiceType == typeof(ITelemetryChannel)).ToArray())
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddSingleton<ITelemetryChannel, NullTelemetryChannel>();
+            services.Configure<ApplicationInsightsServiceOptions>(
+                options =>
+                {
+                    options.EnableAppServicesHeartbeatTelemetryModule = false;
+                    options.EnableHeartbeat = false;
+                    options.EnableQuickPulseMetricStream = false;
+                });
+        }
+
+        return services;
+    }
+}
--- Lombiq.Hosting.Azure.ApplicationInsights/Startup.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Startup.cs
@@ -1,84 +1,37 @@
 using Lombiq.Hosting.Azure.ApplicationInsights.Services;
-using Lombiq.Hosting.Azure.ApplicationInsights.TelemetryInitializers;
-using Microsoft.ApplicationInsights.AspNetCore.Extensions;
-using Microsoft.ApplicationInsights.Channel;
-using Microsoft.ApplicationInsights.DependencyCollector;
-using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using OrchardCore.BackgroundTasks;
-using OrchardCore.Environment.Shell.Configuration;
 using OrchardCore.Modules;
 using System;
-using System.Linq;
 
 namespace Lombiq.Hosting.Azure.ApplicationInsights;
 
 public class Startup : StartupBase
 {
-    private readonly IShellConfiguration _shellConfiguration;
+    private readonly ApplicationInsightsOptions _applicationInsightsOptions;
 
-    public Startup(IShellConfiguration shellConfiguration) =>
-        _shellConfiguration = shellConfiguration;
+    public Startup(IOptions<ApplicationInsightsOptions> applicationInsightsOptions) =>
+        _applicationInsightsOptions = applicationInsightsOptions.Value;
 
     public override void ConfigureServices(IServiceCollection services)
     {
-        services.AddApplicationInsightsTelemetryProcessor<TelemetryFilter>();
-
-        // Since the below AI configuration needs to happen during app startup in ConfigureServices() we can't use an
-        // injected IOptions<T> here but need to directly bind to ApplicationInsightsOptions.
-        var options = new ApplicationInsightsOptions();
-        var configSection = _shellConfiguration.GetSection("Lombiq_Hosting_Azure_ApplicationInsights");
-        configSection.Bind(options);
-        services.Configure<ApplicationInsightsOptions>(configSection);
-
-        services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>(
-            (module, _) => module.EnableSqlCommandTextInstrumentation = options.EnableSqlCommandTextInstrumentation);
-
-        services.ConfigureTelemetryModule<QuickPulseTelemetryModule>(
-            (module, _) => module.AuthenticationApiKey = options.QuickPulseTelemetryModuleAuthenticationApiKey);
-
-        services.AddSingleton<ITelemetryInitializer, UserContextPopulatingTelemetryInitializer>();
-        services.AddSingleton<ITelemetryInitializer, ShellNamePopulatingTelemetryInitializer>();
-        services.Configure<MvcOptions>((options) => options.Filters.Add(typeof(TrackingScriptInjectingFilter)));
-        services.AddScoped<ITrackingScriptFactory, TrackingScriptFactory>();
-
-        if (options.EnableLoggingTestBackgroundTask)
+        if (_applicationInsightsOptions.EnableLoggingTestBackgroundTask)
         {
             services.AddSingleton<IBackgroundTask, LoggingTestBackgroundTask>();
         }
 
-        if (options.EnableBackgroundTaskTelemetryCollection)
+        if (_applicationInsightsOptions.EnableBackgroundTaskTelemetryCollection)
         {
             services.AddScoped<IBackgroundTaskEventHandler, BackgroundTaskTelemetryEventHandler>();
-        }
-
-        if (options.EnableOfflineOperation)
-        {
-            foreach (var descriptor in services.Where(descriptor => descriptor.ServiceType == typeof(ITelemetryChannel)).ToArray())
-            {
-                services.Remove(descriptor);
-            }
-
-            services.AddSingleton<ITelemetryChannel, NullTelemetryChannel>();
-            services.Configure<ApplicationInsightsServiceOptions>(
-                options =>
-                {
-                    options.EnableAppServicesHeartbeatTelemetryModule = false;
-                    options.EnableHeartbeat = false;
-                    options.EnableQuickPulseMetricStream = false;
-                });
         }
     }
 
     public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
     {
-        var options = serviceProvider.GetService<IOptions<ApplicationInsightsOptions>>().Value;
+        var options = serviceProvider.GetRequiredService<IOptions<ApplicationInsightsOptions>>().Value;
 
         if (options.EnableLoggingTestMiddleware) app.UseMiddleware<LoggingTestMiddleware>();
         // There seems to be no way to apply a default filtering to this from code. Going via services.AddLogging() in
