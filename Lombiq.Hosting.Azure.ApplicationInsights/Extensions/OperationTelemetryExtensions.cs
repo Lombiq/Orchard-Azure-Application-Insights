020ae4280b04f32880a0a06cb0f5f08b37d3dc28
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-11-21 15:02:04 +0100

    Adding extension

---
+++ Lombiq.Hosting.Azure.ApplicationInsights/Extensions/OperationTelemetryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+namespace Lombiq.Hosting.Azure.ApplicationInsights.Extensions;
+
+public static class OperationTelemetryExtensions
+{
+    public static void SetAsIgnoredFailure(this OperationTelemetry operationTelemetry)
+    {
+        operationTelemetry.Success = true;
+
+        // Allow us to filter these requests in the portal:
+        operationTelemetry.Properties["IgnoredFailure"] = "true";
+    }
+}
