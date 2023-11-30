b44c07ab50a362388f5155fb3930b06a0172f659
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-11-30 19:40:43 +0100

    Removing spaces

--- Lombiq.Hosting.Azure.ApplicationInsights/Extensions/TelemetryExtensions.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Extensions/TelemetryExtensions.cs
@@ -40,9 +40,9 @@ public static class TelemetryExtensions
             .RequestIgnoreFailureRegex
             ?.IsMatch(requestTelemetry.Url.ToString()) == true;
 
- #pragma warning disable S100 // Making an exception here because 4xx describes it better.
+#pragma warning disable S100 // Making an exception here because 4xx describes it better.
     private static bool IsResult4xx(string code) =>
- #pragma warning restore S100
+#pragma warning restore S100
         int.TryParse(code, out var resultCode) &&
         resultCode is >= 400 and < 500;
 }
