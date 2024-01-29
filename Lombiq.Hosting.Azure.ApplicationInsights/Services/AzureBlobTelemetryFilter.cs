7adafc527be36e8532f8bab62a5231fc43fb511e
Roland Márkus <roland.markus@lombiq.com>
2024-01-29 15:04:31 +0100

    Removing fields.

--- Lombiq.Hosting.Azure.ApplicationInsights/Services/AzureBlobTelemetryFilter.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Services/AzureBlobTelemetryFilter.cs
@@ -11,15 +11,13 @@ namespace Lombiq.Hosting.Azure.ApplicationInsights.Services;
 /// </summary>
 public class AzureBlobTelemetryFilter(ITelemetryProcessor next, IServiceProvider serviceProvider) : ITelemetryProcessor
 {
-    private readonly ITelemetryProcessor _next = next;
-    private readonly IServiceProvider _serviceProvider = serviceProvider;
     private string _parentId;
 
     public void Process(ITelemetry item)
     {
         SetAsIgnoredFailureWhenNeeded(item);
 
-        _next.Process(item);
+        next.Process(item);
     }
 
     private void SetAsIgnoredFailureWhenNeeded(ITelemetry item)
@@ -27,7 +25,7 @@ public class AzureBlobTelemetryFilter(ITelemetryProcessor next, IServiceProvider
         if (item is not DependencyTelemetry { Success: false } dependency) return;
 
         if (dependency.ResultCode == "404" && dependency.Type == "Azure blob" &&
-            dependency.ShouldSetAsIgnoredFailure(_serviceProvider))
+            dependency.ShouldSetAsIgnoredFailure(serviceProvider))
         {
             _parentId = dependency.Context.Operation.ParentId;
             dependency.SetAsIgnoredFailure();
