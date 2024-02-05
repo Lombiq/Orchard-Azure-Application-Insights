e56f22efb559e789453a57e4f45e30908c6205c8
Roland Márkus <roland.markus@lombiq.com>
2024-02-05 15:12:14 +0100

    Moving comment.

--- Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
+++ Lombiq.Hosting.Azure.ApplicationInsights/Services/TelemetryFilter.cs
@@ -14,10 +14,6 @@ public partial class TelemetryFilter : ITelemetryProcessor
 {
     private static readonly List<Regex> _expectedErrors =
     [
-        // Using this generic error message to filter because it is not possible to filter only those cases where it is
-        // meant to happen to throw this error. Also setting CreateContainer to false won't solve this issue, because
-        // Blob Media container is always checked on each tenant activation in OrchardCore
-        // MediaBlobContainerTenantEvents class.
         RequestFailedRegex(),
         ObjectAlreadyExistsRegex(),
     ];
@@ -97,13 +93,17 @@ public partial class TelemetryFilter : ITelemetryProcessor
         return dependency.Data.Contains(mediaBlobStorageContainerName) ? dependency : null;
     }
 
+    // Using this generic error message to filter because it is not possible to filter only those cases where it is
+    // meant to happen to throw this error. Also setting CreateContainer to false won't solve this issue, because
+    // Blob Media container is always checked on each tenant activation in OrchardCore
+    // MediaBlobContainerTenantEvents class.
     [GeneratedRegex(@"Azure\.RequestFailedException: The specified container already exists\.",
-        RegexOptions.Compiled,
+        RegexOptions.IgnoreCase,
         matchTimeoutMilliseconds: 1000)]
     private static partial Regex RequestFailedRegex();
 
     [GeneratedRegex(@"Microsoft\.Data\.SqlClient\.SqlException \(0x80131904\): There is already an object named '.*_Identifiers' in the database\.",
-            RegexOptions.Compiled,
+            RegexOptions.IgnoreCase,
             matchTimeoutMilliseconds: 1000)]
     private static partial Regex ObjectAlreadyExistsRegex();
 }
